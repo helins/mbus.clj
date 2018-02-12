1ded9f7c14e104f8207ca828daeb14872f3fcaec
Adam Helinski <helinski.adam@outlook.com>
2018-02-12 17:32:57 +0100

    Add user namespace
    This namespaces requires all useful namespaces.

---
+++ dev/user.clj
@@ -0,0 +1,25 @@
+(ns user
+
+  "For daydreaming in the repl."
+
+  (:require [clojure.spec.alpha      :as s]
+            [clojure.spec.gen.alpha  :as gen]
+            [clojure.spec.test.alpha :as st]
+            [criterium.core          :as ct]
+            [dvlopt.ex               :as ex]
+            [dvlopt.mbus             :as mbus]
+            [dvlopt.mbus.interop     :as mbus.interop]
+            [dvlopt.mbus.wired       :as mbus.wired]
+            [dvlopt.void             :as void]
+            [taoensso.nippy          :as nippy]))
+
+
+
+
+;;;;;;;;;;
+
+
+(comment
+
+
+  )
